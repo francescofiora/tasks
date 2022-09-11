5759817b9cdc69aef46b40433f9a0a5003a1b7a7
Francesco Fiora <info@francescofiora.it>
2022-09-11 14:08:03 +0100

    Refactoring docker-compose file for manual testing in http

--- docker/phpmyadmin/config.user.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$i = 1;
-$cfg['Servers'][$i]['ssl'] = true;
-$cfg['Servers'][$i]['ssl_key'] = '/etc/certs/tasks-myadmin-key.pem';
-$cfg['Servers'][$i]['ssl_cert'] = '/etc/certs/tasks-myadmin-cert.pem';
-$cfg['Servers'][$i]['ssl_ca'] = '/etc/certs/ca.pem';
-$cfg['Servers'][$i]['ssl_ca_path'] = '/etc/certs/';
